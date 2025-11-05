6dd8ed876e3f57cce686f4540e566149ab4c3439
bielwolf <gabryellobo@gmail.com>
2025-11-05 10:46:33 -0300

    Criando componentes

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,24 +1,24 @@
-import Banner from "componentes/Banner";
-import Card from "componentes/Card";
-import Titulo from "componentes/Titulo";
+import Banner from 'componentes/Banner';
+import styles from './Favoritos.module.css'
+import Titulo from 'componentes/Titulo';
 import videos from 'json/db.json';
-import styles from './Inicio.module.css'
+import Card from 'componentes/Card';
 
-function Inicio() {
+
+function Favoritos () {
     return (
         <>
-            <Banner imagem="home" />
+            <Banner imagem="favoritos" />
             <Titulo>
-                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+                <h1>Meus Favoritos</h1>
             </Titulo>
             <section className={styles.container}>
                 {videos.map((video) => {
                    return <Card {...video} key={video.id} />
                 })}
             </section>
-
         </>
     )
 }
 
-export default Inicio;
+export default Favoritos;
