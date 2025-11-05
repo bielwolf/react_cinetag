7ae6c45e3058c19c0a1da82cf86089875d701939
bielwolf <gabryellobo@gmail.com>
2025-11-05 20:18:50 -0300

    externalizando dados

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,16 +1,22 @@
 import Banner from 'componentes/Banner';
 import styles from './Player.module.css'
 import Titulo from 'componentes/Titulo';
-import videos from 'json/db.json';
 import { useParams } from 'react-router-dom';
 import NaoEncontrada from 'pages/NaoEncontrada';
+import { useEffect, useState } from 'react';
 
 function Player() {
+    const [video, setVideo] = useState();
     const parametros = useParams();
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id);
-    })
 
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/bielwolf/cinetag_api/videos?id=${parametros.id}`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideo(...dados)
+        })
+    }, [])
+   
     if(!video) {
         return <NaoEncontrada />
     }
